6454045cdbe1c1bee8b34abc8d0a23ea51f920a2
Ange <ange.tanon@conserto.pro>
2022-10-31 15:40:28 +0100

    custom Validators

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
@@ -18,8 +19,10 @@ import { MatRadioModule } from '@angular/material/radio';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCheckboxModule,
     MatCardModule,
-    MatRadioModule
+    MatRadioModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule {}
